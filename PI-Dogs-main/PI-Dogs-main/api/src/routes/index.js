d7376a833ac4af0fabb1c16001786d1b6cb910a4
JuanmaMendoza <juanma.mendoza98@hotmail.com>
2023-06-14 12:06:21 -0300

    first commit

---
+++ PI-Dogs-main/PI-Dogs-main/api/src/routes/index.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const axios = require('axios')
+const { Op } = require("sequelize")
+const { API_KEY } = process.env
+const { conn, Raza, Temperamento, RazaTemperamento } = require('../db.js')
+const {
+    getBreeds, getBreedsById, getTemperaments, addBreed, getBreedName
+} = require('./../controllers/index.js')
+
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+
+router.get('/dogs', getBreeds)
+router.get('/dogs/:idRaza', getBreedsById)
+router.get('/temperaments', getTemperaments)
+router.post('/dogs', addBreed)
+router.get('/breedName', getBreedName)
+
+
+
+module.exports = router;
